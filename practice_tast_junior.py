433d42669702a2053b206cd7e8ddac232e3dbde3
Milana <ln1441981@gmail.com>
2026-01-25 18:49:24 +0200

    Добавлен HTML-файл лабораторной №3

---
+++ practice_tast_junior.py
@@ -0,0 +1,12 @@
+# Задача №1
+# Создать список чисел от 1 до 5 и добавить еще одно число в конец списка.
+numbers = [1, 2, 3, 4, 5]
+print("Исходный список:", numbers)
+numbers.append(6)
+print("После добавления 6:", numbers)
+
+number = [9,8,7,6,5,4,3,2]
+print("Исходный список:",number)
+number.append(1)
+number.append(0)
+print("После добавления 0 и 1:",number)
